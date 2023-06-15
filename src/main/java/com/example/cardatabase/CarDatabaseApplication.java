62523910c82af297f3c4d45a523555d59cbe26f9
Hyeongchan Shin <intshc@gmail.com>
2023-06-15 13:50:15 +0900

    Application에서의 로그는 Logger으로

--- src/main/java/com/example/cardatabase/CarDatabaseApplication.java
+++ src/main/java/com/example/cardatabase/CarDatabaseApplication.java
@@ -1,13 +1,21 @@
 package com.example.cardatabase;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class CarDatabaseApplication {
+	private static final Logger logger=
+			LoggerFactory.getLogger(CarDatabaseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarDatabaseApplication.class, args);
+		logger.info("Application Started@@");
+//		log.info("Application Started!!");
 	}
 
 }
