dec9d1d0fe3aec52cd3b26b3643f4801b7dd7769
Hyeongchan Shin <intshc@gmail.com>
2023-06-18 23:06:39 +0900

    Spring data REST 추가(URL parameter 검색)

--- src/main/java/com/example/cardatabase/repository/CarRepository.java
+++ src/main/java/com/example/cardatabase/repository/CarRepository.java
@@ -1,28 +1,18 @@
 package com.example.cardatabase.repository;
 
 import com.example.cardatabase.domain.Car;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource()
 public interface CarRepository extends CrudRepository<Car,Long> {
 
-    //색상으로 자동차를 검색
-    List<Car> findByColor(String color);
+    //브랜드로 자동차 검색
+    List<Car> findByBrand(@Param("brand") String brand);
 
-    //브랜드로 자동차를 검색하고 연도로 정렬
-    List<Car> findByBrandOrderByYearAsc(String brand);
-
-    //sql 문을 이용해 브랜드로 자동차를 검색
-    @Query("select c from Car c where c.brand like %?1")
-    List<Car> findByBrand(String brand);
-
-    Iterable<Car> findAll(Sort sort);
-
-    Page<Car> findAll(Pageable pageable);
-
+    //색상으로 자동차 검색
+    List<Car> findByColor(@Param("color") String color);
 }
