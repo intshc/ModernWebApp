f217cc321a2f282b4059c7b2bd5feee04f7ca860
Hyeongchan Shin <intshc@gmail.com>
2023-06-21 16:21:34 +0900

    빈 등록방식 수정

--- src/main/java/com/example/cardatabase/config/SecurityConfig.java
+++ src/main/java/com/example/cardatabase/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -54,8 +55,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager getAuthenticationManager() throws Exception {
-        return authentication -> authentication;
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
